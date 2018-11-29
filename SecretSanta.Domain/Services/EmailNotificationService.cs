01c6056ffcbfce0f6cbec014228bf0ea516d5fcd
ellingent <38436861+ellingent@users.noreply.github.com>
2018-11-29 08:44:38 -0600

    Oops

--- SecretSanta.Domain/Services/EmailNotificationService.cs
+++ SecretSanta.Domain/Services/EmailNotificationService.cs
@@ -16,7 +16,7 @@ namespace SecretSanta.Domain.Services {
             msg.AddContent(MimeType.Text, string.Format("Hi {0},", person.FirstName));
             msg.AddContent(MimeType.Text, string.Format("It's almost Christmas, which means it's time for the gift exchange!You have been automatically assigned to purchase a gift for {0}.", person.Giftee.FirstName));
 
-            var client = new SendGridClient("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");
+            var client = new SendGridClient("");
             client.SendEmailAsync(msg);
         }
     }
